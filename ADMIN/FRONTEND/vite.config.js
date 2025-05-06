c3c5287571690348a70e62f6a3715d0aa04a7fd0
vivek-outdid <vivek@outdidtech.com>
2025-05-06 12:06:53 +0530

    ADMIN - Docker setup for admin is completed

--- ADMIN/FRONTEND/vite.config.js
+++ ADMIN/FRONTEND/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000, 
     strictPort: true, 
     proxy: {
       '/api': {
-        target: 'http://192.168.1.44:8383',
+        target: 'http://admin-backend:3001',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
